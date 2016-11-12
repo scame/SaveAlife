13d46a0af8f27860db3d4bf5836a359bd3d4c5f2
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-11-12 16:38:55 +0200

    added a simple mapbox map

--- app/src/main/java/com/krestone/savealife/SaveAlifeApplication.java
+++ app/src/main/java/com/krestone/savealife/SaveAlifeApplication.java
@@ -3,10 +3,13 @@ package com.krestone.savealife;
 
 import android.app.Application;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+
 public class SaveAlifeApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        MapboxAccountManager.start(this, getString(R.string.default_access_token));
     }
 }
--- app/src/main/java/com/krestone/savealife/presentation/fragments/MapFragment.java
+++ app/src/main/java/com/krestone/savealife/presentation/fragments/MapFragment.java
@@ -1,8 +1,67 @@
 package com.krestone.savealife.presentation.fragments;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.krestone.savealife.R;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MapFragment extends Fragment {
 
+    @BindView(R.id.mapview)
+    MapView mapView;
+
+    private MapboxMap mapboxMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.map_fragment_layout, container, false);
+
+        ButterKnife.bind(this, fragmentView);
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(mapboxMap1 -> MapFragment.this.mapboxMap = mapboxMap1);
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 }
