7c8d8953dd07a0e63bad584aa17fe49342102f09
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-11-16 19:04:18 +0200

    added dashboard layout

--- app/src/main/java/com/krestone/savealife/presentation/fragments/ChatsFragment.java
+++ app/src/main/java/com/krestone/savealife/presentation/fragments/ChatsFragment.java
@@ -1,20 +1,7 @@
 package com.krestone.savealife.presentation.fragments;
 
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.krestone.savealife.R;
 
 public class ChatsFragment extends Fragment {
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sign_up_layout, container, false);
-    }
 }
--- app/src/main/java/com/krestone/savealife/presentation/fragments/DashboardFragment.java
+++ app/src/main/java/com/krestone/savealife/presentation/fragments/DashboardFragment.java
@@ -1,8 +1,20 @@
 package com.krestone.savealife.presentation.fragments;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.krestone.savealife.R;
 
 public class DashboardFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dashboard_layout, container, false);
+    }
 }
