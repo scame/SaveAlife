4fe75e4d87461cfc906f7936747a5aa578f36da3
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-12-02 01:14:13 +0200

    added active/inactive SOS state transitions

--- app/src/main/java/com/krestone/savealife/presentation/fragments/DashboardFragment.java
+++ app/src/main/java/com/krestone/savealife/presentation/fragments/DashboardFragment.java
@@ -7,14 +7,72 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.krestone.savealife.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class DashboardFragment extends Fragment {
+
+    @BindView(R.id.root_dashboard_container)
+    RelativeLayout rootContainer;
+
+    @BindView(R.id.dashboard_location_tv)
+    TextView locationTv;
+
+    @BindView(R.id.dashboard_map_btn)
+    ImageButton mapButton;
+
+    @BindView(R.id.sos_text)
+    TextView sosText;
+
+    @BindView(R.id.sos_btn)
+    ImageView sosButton;
+
+    @BindView(R.id.driver_mode_switch)
+    Switch driverModeSwitch;
+
+    @BindView(R.id.event_details_btn)
+    Button eventDetailsBtn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dashboard_layout, container, false);
+        View fragmentView = inflater.inflate(R.layout.dashboard_layout, container, false);
+        ButterKnife.bind(this, fragmentView);
+
+        return fragmentView;
+    }
+
+
+    @OnClick(R.id.sos_btn)
+    public void onClick(View view) {
+        if (sosButton.getTag() == null) {
+            initActiveSosState();
+        } else {
+            initInactiveSosState();
+        }
+    }
+
+    private void initActiveSosState() {
+        sosButton.setImageDrawable(getResources().getDrawable(R.drawable.black_background));
+        sosButton.setTag(new Object());
+        rootContainer.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        sosText.setText(getString(R.string.sos_active_text));
+    }
+
+    private void initInactiveSosState() {
+        sosButton.setImageDrawable(getResources().getDrawable(R.drawable.placeholder));
+        sosButton.setTag(null);
+        sosText.setText(getString(R.string.sos_inactive_text));
+        rootContainer.setBackgroundColor(getResources().getColor(android.R.color.background_light));
     }
 }
