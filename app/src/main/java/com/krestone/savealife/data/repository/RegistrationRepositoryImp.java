75bd39153a433a042b071258e0a6176de95c1700
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-12-18 21:50:48 +0200

    added entry info caching

--- app/src/main/java/com/krestone/savealife/data/repository/RegistrationRepositoryImp.java
+++ app/src/main/java/com/krestone/savealife/data/repository/RegistrationRepositoryImp.java
@@ -1,6 +1,11 @@
 package com.krestone.savealife.data.repository;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.krestone.savealife.R;
 import com.krestone.savealife.data.entities.requests.PersonalInfoHolder;
 import com.krestone.savealife.data.entities.requests.PhoneNumberHolder;
 import com.krestone.savealife.data.entities.requests.VerificationHolder;
@@ -13,8 +18,11 @@ public class RegistrationRepositoryImp implements RegistrationRepository {
 
     private ServerApi serverApi;
 
-    public RegistrationRepositoryImp(ServerApi serverApi) {
+    private Context context;
+
+    public RegistrationRepositoryImp(ServerApi serverApi, Context context) {
         this.serverApi = serverApi;
+        this.context = context;
     }
 
     @Override
@@ -29,6 +37,22 @@ public class RegistrationRepositoryImp implements RegistrationRepository {
 
     @Override
     public Single<ResponseBody> sendPersonalInfo(PersonalInfoHolder personalInfoHolder) {
-        return serverApi.sendPersonalInfo(personalInfoHolder).toSingle();
+        return serverApi.sendPersonalInfo(personalInfoHolder).toSingle()
+                .doOnSuccess(responseBody -> cacheEntryInfo(personalInfoHolder));
+    }
+
+    private void cacheEntryInfo(PersonalInfoHolder personalInfoHolder) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        editor.putString(getString(R.string.password), personalInfoHolder.getPassword()).apply();
+        editor.putString(getString(R.string.phone_number), personalInfoHolder.getPhoneNumber()).apply();
+        editor.putString(getString(R.string.first_name), personalInfoHolder.getFirstName()).apply();
+        editor.putString(getString(R.string.last_name), personalInfoHolder.getLastName()).apply();
+        editor.putString(getString(R.string.current_token), personalInfoHolder.getCurrentToken()).apply();
+        editor.putString(getString(R.string.med_qualification), personalInfoHolder.getMedicalQualification()).apply();
+    }
+
+    private String getString(int stringId) {
+        return context.getString(stringId);
     }
 }
